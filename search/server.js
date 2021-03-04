04ba03b71ed1bfa859edca857389bcbbfa58ff70
Sebastian Russo <russo.sebastian@gmail.com>
2021-03-04 14:39:16 -0500

    config file

--- search/server.js
+++ search/server.js
@@ -86,5 +86,6 @@ app.post('/videos', async (req, res) => {
 /******************** LIST TO PORT  *********************/
 
 const port = process.env.PORT || 3001;
+console.log(process.env)
 app.listen(port, () => console.log(`Listing on port ${port}`));
 
